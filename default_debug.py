199393d1460e493d673cf6062a40002eb5655394
marduklev <36476590+marduklev@users.noreply.github.com>
2022-03-22 14:20:11 +0100

    misc

--- default_debug.py
+++ default_debug.py
@@ -11,18 +11,24 @@ import sys
 
 def decode(source=None,property='decoded_string'):
     result = urllib.parse.unquote(source)
+    DIALOG.textviewer('encode ','  source: %s  \n  result: %s ' % (source,result))
     xbmc.executebuiltin("SetProperty(%s,%s,home)" % (property,result))
     # log(loginfo)
   
 def encode(source=None,property='encoded_string'):
+    # source = 'Smashin"""""g Pum"§$pkins über äll>is' 
     result = urllib.parse.quote(source.encode())
+    DIALOG.textviewer('encode ','  source: %s  \n  result: %s ' % (source,result))
     xbmc.executebuiltin("SetProperty(%s,%s,home)" % (property,result))   
     # log(loginfo)
 
 def checkexist(file=None,property='filesearch_result'):
     if xbmcvfs.exists(file):
         xbmc.executebuiltin("SetProperty(%s,%s,home)" % (property,file))
+    else:
+        xbmc.executebuiltin('notification(log Nothing Found in,  %s)' % file)    
     # log(loginfo)
+    
 
 def main():
     if ACTION == 'checkexist':
@@ -45,10 +51,11 @@ def log():
     
 if __name__ == '__main__':
     
+    # CONSTANTS
     ADDON = xbmcaddon.Addon()
     ADDON_ID = ADDON.getAddonInfo('id')
     DIALOG = xbmcgui.Dialog()
-    
+    #
     ARGS = sys.argv[1:]
     ACTION = sys.argv[1].split('action=')[1]
     # action.keyname - almost irrelevant codewise, just easier to set up script calls
@@ -58,6 +65,7 @@ if __name__ == '__main__':
     
     if sys.argv[3]:
         KNAME3 = sys.argv[3].split('=')[0]
+        # KNAMEVALUE3 = sys.argv[3].split('=')[1][2:-2]
         KNAMEVALUE3 = sys.argv[3].split(f'{KNAME3}=')[1]
     
     main()
